8afb7cb115419dd97eaecf6f8b5071c6db8b32d6
Alwyn Tan <alwyn.tan@data.gov.sg>
2017-07-20 14:55:00 +0800

    Add a searchbox and localtoc to html_sidebars, as well as a title
    TODO: Carve up the document contents into sections so that search actually works

--- conf.py
+++ conf.py
@@ -91,11 +91,19 @@ html_theme = 'alabaster'
 #
 # html_theme_options = {}
 
+html_title = 'Law Society of Singapore - Clinic Manual'
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_sidebars = {
+  '**': [
+    'searchbox.html',
+    'localtoc.html'
+  ],
+}
 
 # -- Options for HTMLHelp output ------------------------------------------
 
@@ -152,6 +160,3 @@ texinfo_documents = [
      author, 'lawsoc-clinic-manual', 'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-
